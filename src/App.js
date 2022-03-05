d77d867cf8ee4e021026ed7d21b7ab3b41ab5e61
Your Name <youremail@yourdomain.com>
2022-03-05 10:08:12 +0100

    this is first test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import image from './Images/A11.png';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div  style={{border:"solid 1px black " , maxwidth:"100vw"}}>My chekpoint</div>
+     <br />
+     <br />
+     <h1 className="title red" style={{color:"red"}}>chaaben bilel</h1>
+      <img src={image} alt="imginsrc" />
+      <br />
+      <br />
+      <img src="/Imagess/A6.png" alt="imginpublic" />
+     <iframe width="320" height="240"  src="https://www.youtube.com/embed/olJOZp57FWM" title="YouTube video player" 
+     frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" frameborder="0"></iframe>
+     
+      
+
+ 
+
+
     </div>
   );
 }
